c5a13d0f9b4b6865b5690fe8dc45ba0063bab5a3
Josh Zacharias <josh.owie@gmail.com>
2015-05-19 21:35:10 -0700

    Facet modification

--- src/main/java/org/terasology/tutorialWorldGeneration/TutorialWorldGenerator.java
+++ src/main/java/org/terasology/tutorialWorldGeneration/TutorialWorldGenerator.java
@@ -33,6 +33,7 @@ public class TutorialWorldGenerator extends BaseFacetedWorldGenerator {
         return new WorldBuilder(seed)
                 .addProvider(new SurfaceProvider())
                 .addProvider(new SeaLevelProvider(0))
+                .addProvider(new MountainsProvider())
                 .addRasterizer(new TutorialWorldRasterizer());
     }
 }
